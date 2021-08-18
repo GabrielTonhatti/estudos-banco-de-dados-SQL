1d912260227a021cde3e9411d5d1c6fd540baaf2
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-08-17 23:37:58 -0300

    add new types

---
+++ tipos-dados/tipo-numerico.sql
@@ -0,0 +1,25 @@
+USE tipos_dados;
+
+DROP TABLE aluno;
+
+CREATE TABLE aluno
+(
+    matricula         CHAR(10)     NOT NULL,
+    nome              VARCHAR(120) NOT NULL,
+    nome_social       VARCHAR(80),
+    semestre          TINYINT,
+    creditos_cursados SMALLINT,
+    media_geral       DECIMAL(4, 2)
+);
+
+INSERT INTO aluno
+VALUES ("202106A6F7", -- matricula
+        "Marlos Pinheiro Borges", -- nome
+        "Marlin", -- nome_social
+        3, -- semestre
+        56, -- creditos_cursados
+        8.7 -- media_geral
+       );
+
+SELECT *
+FROM aluno;
